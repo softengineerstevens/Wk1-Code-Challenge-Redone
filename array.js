643b8bc879441f4899d063bd907407719bdbdb30
softengineerstevens <softengineerstevens@gmail.com>
2024-07-01 11:05:20 +0300

    updated submission

---
+++ array.js
@@ -0,0 +1,7 @@
+const fruits=['banana','mango','pineapple','oranges','avacado'];
+console.log(fruits.splice(2,0,'tomato'));
+
+
+// const newArray=fruits.slice();
+// fruits.unshift('guava') 
+// console.log(newArray); 
